168ee0bd306df90b02ab0ca788ed693df123d5ef
«Andrey» <khomehko@mail.ru>
2022-08-30 03:15:10 +0300

    h46

--- h46/Program.cs
+++ h46/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+
+Console.Write("Введите число M : ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N : ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = Fill2DArr(M, N, -10, 10);
+
+PrintArray(array); // Вызов метода печати массива
+
+
+// Метод возвращает двумерный массив [,] на size элементов 
+int[,] Fill2DArr(int rowsCount, int columnsCount, int lefts, int rights)
+{
+    int[,] array = new int[rowsCount, columnsCount];
+
+    Random random = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(lefts, rights);
+        }
+    }
+    {
+
+    }
+    return array;
+}
+// Матод печати двумерного массива
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+
+}
