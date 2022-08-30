415642294a37845fab13f34599e2566f0f12f8d6
«Andrey» <khomehko@mail.ru>
2022-08-30 15:07:29 +0300

    h50

---
+++ h50/Program.cs
@@ -0,0 +1,66 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1 7 -> такого числа в массиве нет
+
+Console.Write("Введите число M : ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N : ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = Fill2DArr(M, N, -10, 10);
+
+PrintArray(array); // Вызов метода печати массива
+
+Console.Write("Введите номер строки : ");
+int rowNum = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите номер столбца : ");
+int columnNum = Convert.ToInt32(Console.ReadLine());
+
+if ((rowNum > array.GetLength(0)) | (columnNum > array.GetLength(1)) | (rowNum < 1) | (columnNum < 1))
+{
+    
+    Console.WriteLine("такого числа в массиве нет");
+}
+else
+{
+    Console.Write("Значение элемента равно :");
+    Console.Write(array[rowNum - 1, columnNum - 1]);
+}
+
+// Метод возвращает двумерный массив [,] на size элементов 
+int[,] Fill2DArr(int rowsCount, int columnsCount, int lefts, int rights)
+{
+    int[,] array = new int[rowsCount, columnsCount];
+
+    Random random = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(lefts, rights);
+        }
+    }
+    {
+
+    }
+    return array;
+}
+// Матод печати двумерного массива
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+
+}
